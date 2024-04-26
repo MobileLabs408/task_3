af4fd1f79a3b0e9464fd6a4415d758f848a054f6
Carl Larsson <130466691+Illuminy00@users.noreply.github.com>
2024-04-26 12:19:59 +0200

    Delete task_3/A_star.m

--- task_3/A_star.m
+++
@@ -1,9 +0,0 @@
-%==========================================================================
-% Author: Carl Larsson
-% Description: A*
-% Date: 2024-04-11
-
-% This software is licensed under the MIT License
-% Refer to the LICENSE file for details
-%==========================================================================
-
