25d5d2427e74a7ca589d6778a2c63dad6ca6039e
Carl Larsson <130466691+Illuminy00@users.noreply.github.com>
2024-04-27 09:46:03 +0200

    moved iterative part to own function, comment changes
    moved iterative part to its own function, A_star_unknown, and old A_star_unknown is now modified_A_star. comment changes

--- task_3/a_star_unknown/A_star_unknown.m
+++ task_3/a_star_unknown/A_star_unknown.m
@@ -1,130 +1,50 @@
 %==========================================================================
 % Author: Carl Larsson
-% Description: Modified A* used for UNKNOWN environments
-% Date: 2024-04-25
+% Description: A* used for UNKNOWN environments, itterativly runs a
+% modified version of A*, each time new infromation is gathered, update map
+% Date: 2024-04-27
 
 % This software is licensed under the MIT License
 % Refer to the LICENSE file for details
 %==========================================================================
 
-function [path, obstacle_position, current_position, push, pop] = A_star_unknown(map, created_map, start_position, goal_position)
-    % used keeping track of the number of pushes and pops 
+function [path, created_map, push, pop] = A_star_unknown(map, start_position, goal_position)
+    % Initially we think map is just free space
+    % We assume that we know the size of the map space and its boundaries
+    [map_rows, map_columns] = size(map);
+    created_map = zeros(map_rows,map_columns);
+    % Our current position in the begining is the start position
+    current_position = start_position;
+    % Temporary value to just start the while loop
+    obstacle_position = 0; 
+    path = [];
     push = 0;
     pop = 0;
-
-    % Define movement cost
-    vertical_horizontal_cost = 1;
-    diagonal_cost = sqrt(2);
-
-    % Start node
-    % g is cost-so-far, h is heuristic, and f is estimated-total-cost which is f = g+h
-    start_node = struct('position', start_position, 'g', 0, 'f', A_star_heuristic(start_position, goal_position), 'parent', []);
-
-    % Initialize open and closed lists
-    % Nodes that are yet to be explored (expanded)
-    open_list = [start_node];
-    % Push start node
-    push = push + 1;
-    % Nodes we have expanded
-    closed_list = [];
-
-    % Get dimensions of map
-    [map_rows, map_columns] = size(map);
-
-    % A*
-    % Continue while we have unexplored nodes
-    while ~isempty(open_list)
-        % Find unexplored (unexpanded) node with lowest estimate total cost (f)
-        [~, current_node_idx] = min([open_list.f]);
-        % Pop
-        current_node = open_list(current_node_idx);
-        current_position = current_node.position;
-        pop = pop + 1;
-
-        % Move current node to closed list
-        closed_list = [closed_list; current_node.position];
-
-        % Check if goal is reached
-        if isequal(current_node.position, goal_position)
-            % If we have reached goal, we still need to set value of
-            % obstacle_position, so we set it to NaN so it can easily be
-            % checked
-            obstacle_position = NaN;
-            path = reconstruct_path(current_node);
-            return;
-        end
-        
-        % Get neighboring nodes of current node
-        neighbors = get_neighboring_nodes(current_node.position);
-
-        % Check all neighboring nodes
-        for i = 1:size(neighbors, 1)
-            neighbor_pos = neighbors(i, :);
-            
-            % Skip if neighbor is already in closed list
-            if any(ismember(closed_list, neighbor_pos, 'rows'))
-                continue;
-            end
-
-            % If we encounter edge of maze or obstacles
-            if any(neighbor_pos < 1) || (neighbor_pos(1) > map_rows) || (neighbor_pos(2) > map_columns) || (map(neighbor_pos(1), neighbor_pos(2)) == inf)
-                % We are aware of the obstacle according to our created map, 
-                % so we continue as usual by skipping it
-                if (created_map(neighbor_pos(1), neighbor_pos(2)) == inf)
-                    continue;
-                % If neighbor is outside map boundaries or an obstacle
-                % which we are unaware of (haven't seen before) and is not
-                % on our map
-                % return so map can be updated with this new information
-                else
-                    % Mark the neighbor as an obstacle and return it so that
-                    % map can be updated with this new information
-                    obstacle_position = neighbor_pos;
-                    % Return path so far
-                    path = reconstruct_path(current_node);
-                    return;
-                end
-            end
-            
-            % Determine movement cost
-            % If neighbor is change in both x and y, then we have moved diagonally
-            if abs(neighbor_pos(1) - current_node.position(1)) == 1 && abs(neighbor_pos(2) - current_node.position(2)) == 1
-                % g is cost-so-far
-                neighbor_g = current_node.g + diagonal_cost;
-            else
-                % g is cost-so-far
-                neighbor_g = current_node.g + vertical_horizontal_cost;
-            end
-            
-            % Estimated total cost f = g + h
-            neighbor_f = neighbor_g + A_star_heuristic(neighbor_pos, goal_position);
-            
-            % Create neighbor node
-            neighbor_node = struct('position', neighbor_pos, 'g', neighbor_g, 'f', neighbor_f, 'parent', current_node);
-
-            % If neighbor is not in open list, add it
-            if ~any(arrayfun(@(x) isequal(x, neighbor_node), open_list))
-                open_list = [open_list; neighbor_node];
-                % Push
-                push = push + 1;
-            % Else if it has lower f value, we have found a better path so
-            % we update
-            elseif any(arrayfun(@(x) isequal(x, neighbor_node), open_list)) && open_list(find(arrayfun(@(x) isequal(x, neighbor_node), open_list), 1)).f > neighbor_node.f
-                open_list(find(arrayfun(@(x) isequal(x, neighbor_node), open_list), 1)) = neighbor_node;
-            end
-                
-            % Add neighbor to closed list
-            closed_list = [closed_list; neighbor_pos];
-
-        end
-
-        % Remove current node from open list
-        open_list(current_node_idx) = [];
-
-    end
     
-    % If no path is found
-    disp("No path found.")
-    path = [];
-
+    % NaN is only returned when goal has been reached
+    while ~isnan(obstacle_position)
+        % Itterativly run the modified A*, each time starting from current
+        % position (in the beginning its start position), always with goal
+        % goal_position, reschedule each time new information is gained
+        % and itterativly update map with the new information we gained
+        [temp_path, obstacle_position, new_position, temp_push, temp_pop] = modified_A_star(map, created_map, current_position, goal_position);
+        % Set current_position to the new position we have moved to and are
+        % gonna start planing from next itteration
+        current_position = new_position;
+        % Unless we returned because we found goal, then we returned because 
+        % we found a new obstacle and we need to update the map regarding it
+        if ~isnan(obstacle_position)
+            created_map(obstacle_position(1), obstacle_position(2)) = inf;
+        end
+        % Path is empty if A* can't find a path to goal
+        if isempty(temp_path)
+            break;
+        else
+            % Combine path from all itterations to gain path from start to
+            % goal
+            path = [path; temp_path];
+        end
+        push = push + temp_push;
+        pop = pop + temp_pop;
+    end
 end
---
+++ task_3/a_star_unknown/modified_A_star.m
@@ -0,0 +1,134 @@
+%==========================================================================
+% Author: Carl Larsson
+% Description: Modified A* used for UNKNOWN environments
+% Date: 2024-04-25
+
+% This software is licensed under the MIT License
+% Refer to the LICENSE file for details
+%==========================================================================
+
+function [path, obstacle_position, current_position, push, pop] = modified_A_star(map, created_map, start_position, goal_position)
+    % used keeping track of the number of pushes and pops 
+    push = 0;
+    pop = 0;
+
+    % Define movement cost
+    vertical_horizontal_cost = 1;
+    diagonal_cost = sqrt(2);
+
+    % Start node
+    % g is cost-so-far, h is heuristic, and f is estimated-total-cost which is f = g+h
+    start_node = struct('position', start_position, 'g', 0, 'f', A_star_heuristic(start_position, goal_position), 'parent', []);
+
+    % Initialize open and closed lists
+    % Nodes that are yet to be explored (expanded)
+    open_list = [start_node];
+    % Push start node
+    push = push + 1;
+    % Nodes we have expanded
+    closed_list = [];
+
+    % Get dimensions of map
+    % This will be identical to created map dimensions since we assume we
+    % have knowledge of the size of the map and its edges
+    [map_rows, map_columns] = size(map);
+
+    % A*
+    % Continue while we have unexplored nodes
+    while ~isempty(open_list)
+        % Find unexplored (unexpanded) node with lowest estimate total cost (f)
+        [~, current_node_idx] = min([open_list.f]);
+        % Pop current node
+        current_node = open_list(current_node_idx);
+        current_position = current_node.position;
+        pop = pop + 1;
+
+        % Move current node to closed list
+        closed_list = [closed_list; current_node.position];
+
+        % Check if goal is reached
+        if isequal(current_node.position, goal_position)
+            % If we have reached goal, we still need to set value of
+            % obstacle_position, so we set it to NaN so it can easily be
+            % checked
+            obstacle_position = NaN;
+            path = reconstruct_path(current_node);
+            return;
+        end
+        
+        % Get neighboring nodes of current node
+        neighbors = get_neighboring_nodes(current_node.position);
+
+        % Check all neighboring nodes
+        for i = 1:size(neighbors, 1)
+            neighbor_pos = neighbors(i, :);
+            
+            % Skip if neighbor is already in closed list
+            if any(ismember(closed_list, neighbor_pos, 'rows'))
+                continue;
+            end
+
+            % If we encounter edge of maze or obstacles
+            if any(neighbor_pos < 1) || (neighbor_pos(1) > map_rows) || (neighbor_pos(2) > map_columns) || (map(neighbor_pos(1), neighbor_pos(2)) == inf)
+                % We are aware of the obstacle according to our created map, 
+                % so we continue as usual by skipping it.
+                % We also assume we have knowledge of the size of the map
+                % and its edges, if we encounter edge we also just skip
+                if any(neighbor_pos < 1) || (neighbor_pos(1) > map_rows) || (neighbor_pos(2) > map_columns) || (created_map(neighbor_pos(1), neighbor_pos(2)) == inf)
+                    continue;
+                % If neighbor is an obstacle which we are unaware of 
+                % (haven't seen before) and is not on our created map
+                % return so map can be updated with this new information
+                % and we can reschedule with regards to new information
+                else
+                    % Mark the neighbor as an obstacle and return it so that
+                    % map can be updated with this new information
+                    obstacle_position = neighbor_pos;
+                    % Return path so far
+                    path = reconstruct_path(current_node);
+                    return;
+                end
+            end
+            
+            % Determine movement cost
+            % If neighbor is change in both x and y, then we have moved diagonally
+            if abs(neighbor_pos(1) - current_node.position(1)) == 1 && abs(neighbor_pos(2) - current_node.position(2)) == 1
+                % g is cost-so-far
+                neighbor_g = current_node.g + diagonal_cost;
+            else
+                % g is cost-so-far
+                neighbor_g = current_node.g + vertical_horizontal_cost;
+            end
+            
+            % Estimated total cost f = g + h
+            neighbor_f = neighbor_g + A_star_heuristic(neighbor_pos, goal_position);
+            
+            % Create neighbor node
+            neighbor_node = struct('position', neighbor_pos, 'g', neighbor_g, 'f', neighbor_f, 'parent', current_node);
+
+            % If neighbor is not in open list, add it
+            if ~any(arrayfun(@(x) isequal(x, neighbor_node), open_list))
+                % Push neighbor node
+                open_list = [open_list; neighbor_node];
+                push = push + 1;
+            % Else if it has lower f value, we have found a better path so
+            % we update
+            elseif any(arrayfun(@(x) isequal(x, neighbor_node), open_list)) && open_list(find(arrayfun(@(x) isequal(x, neighbor_node), open_list), 1)).f > neighbor_node.f
+                open_list(find(arrayfun(@(x) isequal(x, neighbor_node), open_list), 1)) = neighbor_node;
+            end
+                
+            % Add neighbor to closed list
+            closed_list = [closed_list; neighbor_pos];
+
+        end
+
+        % Remove current node from open list
+        open_list(current_node_idx) = [];
+
+    end
+    
+    % If no path is found
+    disp("No path found.")
+    path = [];
+
+end
