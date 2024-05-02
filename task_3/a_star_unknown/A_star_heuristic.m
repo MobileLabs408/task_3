16c47b1561bfb3b777cda361a1bb0df480c82001
Carl Larsson <130466691+Illuminy00@users.noreply.github.com>
2024-05-02 11:24:45 +0200

    comments
    comments

--- task_3/a_star_unknown/A_star_heuristic.m
+++ task_3/a_star_unknown/A_star_heuristic.m
@@ -16,10 +16,11 @@ function heuristic = A_star_heuristic(current_position, goal_position)
     %heuristic = 0;
 
     % Diagonal distance
+    %
     D = 1;
     D2 = 1;
     dx = abs(current_position(1) - goal_position(1));
     dy = abs(current_position(2) - goal_position(2));
     heuristic = D*(dx + dy) + (D2 - 2*D) * min(dx, dy);
-
+    %}
 end
